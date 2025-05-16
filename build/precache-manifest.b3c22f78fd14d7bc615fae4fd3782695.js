3a42bdc47629d2691f2c35c041bf02ef6ce2d7e9
NJames805 <nathanaeljames805@email.com>
2025-05-15 23:51:07 -0400

    Deploying Frontend

---
+++ build/precache-manifest.b3c22f78fd14d7bc615fae4fd3782695.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3632fa5841754facca780dd116be84ae",
+    "url": "/webdev-final-project/index.html"
+  },
+  {
+    "revision": "80ba6e04a8251aca7771",
+    "url": "/webdev-final-project/static/css/main.1dfaee75.chunk.css"
+  },
+  {
+    "revision": "0119492c5867157b6efa",
+    "url": "/webdev-final-project/static/js/2.3ca6a43e.chunk.js"
+  },
+  {
+    "revision": "7a51affaa6c380edc87d9b28e02eee52",
+    "url": "/webdev-final-project/static/js/2.3ca6a43e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4a383a13c2d54d9ef7d",
+    "url": "/webdev-final-project/static/js/3.d80ec6ad.chunk.js"
+  },
+  {
+    "revision": "80ba6e04a8251aca7771",
+    "url": "/webdev-final-project/static/js/main.da96beac.chunk.js"
+  },
+  {
+    "revision": "d8b2bc1af65c9efaa845",
+    "url": "/webdev-final-project/static/js/runtime-main.d16308dc.js"
+  }
+]);
